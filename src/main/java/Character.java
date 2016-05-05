f944c7b874309c357f88351309b7d88d4e73aa5d
yujhen <qwa33096@gmail.com>
2016-05-05 13:09:49 +0800

    add lab code

--- src/main/java/Character.java
+++ src/main/java/Character.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.ArrayList;
+
 import processing.core.PApplet;
 
 /**
@@ -9,15 +11,36 @@ import processing.core.PApplet;
 public class Character {
 	
 	private MainApplet parent;
-
-	public Character(MainApplet parent){
-
+	public float x, y, radius;
+	private String name;
+	private ArrayList<Character> targets;
+	private String color;
+	public Character(MainApplet parent, String name, String color, float x, float y){
+		this.x = x;
+		this.y = y;
+		this.color = color;
+		this.name = name;
+		this.parent = parent;
+		this.targets = new ArrayList<Character>();
 		this.parent = parent;
 		
 	}
 
 	public void display(){
+		this.parent.fill(210, 2, 11);
+		this.parent.ellipse(x, y, 20, 20);
+		
+		//this.parent.textSize(26);
+		//this.parent.fill(255);
+		//this.parent.text(name, x-name.length()*10+5, y+10);
+	}
 
+	
+	public void addTarget(Character target){
+		this.targets.add(target);
 	}
 	
+	public ArrayList<Character> getTargets(){
+		return this.targets;
+	}
 }
--- src/main/java/MainApplet.java
+++ src/main/java/MainApplet.java
@@ -1,32 +1,65 @@
 package main.java;
 
 import processing.core.PApplet;
-
+import java.util.ArrayList;
+import processing.core.PApplet;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
 /**
 * This class is for sketching outcome using Processing
 * You can do major UI control and some visualization in this class.  
 */
 @SuppressWarnings("serial")
 public class MainApplet extends PApplet{
+	JSONObject data;
+	JSONArray nodes, links;
+	private ArrayList<Character> characters;
+	
 	private String path = "main/resources/";
 	private String file = "starwars-episode-1-interactions.json";
 	
 	private final static int width = 1200, height = 650;
 	
 	public void setup() {
-
+		characters = new ArrayList<Character>();
 		size(width, height);
 		smooth();
 		loadData();
 		
 	}
-
+	public void keyPressed(){
+		if(keyCode==32)
+			setup();
+	}
 	public void draw() {
-
+		stroke(60, 119, 119);
+		strokeWeight(4);
+		
+		for(Character character: this.characters){
+			for(Character target: character.getTargets())
+				line(character.x, character.y, target.x, target.y);
+		}
+		
+		for(Character character : characters)
+			character.display();
 	}
 
 	private void loadData(){
+		int x = 50;
+		data = loadJSONObject(path+file);
+		nodes = data.getJSONArray("nodes");
+		links = data.getJSONArray("links");
+		for(int i=0; i<nodes.size(); i++){
+			JSONObject node = nodes.getJSONObject(i);
+			Character a = new Character(this, node.getString("name"), node.getString("colour"), i/10 + 10, x ) ;
+			x+=50;
+			characters.add(a);
+		}
 
+		for(int i=0; i<links.size(); i++){
+			JSONObject link = links.getJSONObject(i);
+			characters.get(link.getInt("source")).addTarget(characters.get(link.getInt("target")));
+		}
 	}
 
 }
